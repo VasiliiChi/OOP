df09a721e0e7e134d3f18d3120aadf5079c364cd
VasiliiChibisov <chibisowasya@gmail.com>
2022-03-29 23:15:21 +0300

    Добавьте файлы проекта.

---
+++ Class and Struct/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Class_and_Struct
+{
+	namespace FirstApp
+	{
+		class Human
+		{
+			// Поля класса
+			public string name;
+			public int age;
+
+			// Метод класса
+			public void Greetings()
+			{
+				Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+			}
+		}
+
+		struct Animal
+		{
+			// Поля структуры
+			public string type;
+			public string name;
+			public int age;
+
+			// Метод структуры
+			public void Info()
+			{
+				Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+			}
+		}
+		class Program
+    {
+        static void Main(string[] args)
+        {
+				Human human = new Human();
+				human.Greetings();
+
+				human.name = "Дмитрий";
+				human.age = 23;
+				human.Greetings();
+
+
+				Console.ReadKey();
+			}
+    }
+}
