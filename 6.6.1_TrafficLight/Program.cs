a9e9edb7d4f2b4a62af3696be85e5aa698f7e2b7
VasiliiChibisov <chibisowasya@gmail.com>
2022-03-31 22:25:44 +0300

    Добавили проект (задание 6.6.1) TrafficLight

---
+++ 6.6.1_TrafficLight/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _6._6._1_TrafficLight
+{
+    class Program
+    {
+        class TrafficLight
+        {
+            private void ChangeColor(string color)
+            {
+
+            }
+
+            public string GetColor()
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+            {
+
+
+
+                Console.ReadKey();
+            }
+        }
+    }
--- Class and Struct/Program.cs
+++ Class and Struct/Program.cs
@@ -2,68 +2,87 @@
 
 namespace Class_and_Struct
 {
-	namespace FirstApp
-	{
-		class Human
-		{
-			// Поля класса
-			public string name;
-			public int age;
 
-			// Метод класса
-			public void Greetings()
-			{
-				Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
-			}
-			// Конструктор 1
-			public Human()
-			{
-				name = "Неизвестно";
-				age = 20;
-			}
-			// Конструктор 2
-			public Human(string n)
-			{
-				name = n;
-				age = 20;
-			}
-			// Конструктор 3
-			public Human(string n, int a)
-			{
-				name = n;
-				age = a;
-			}
+    class Human
+    {
+        // Поля класса
+        public string name;
+        public int age;
 
-			struct Animal
-			{
-				// Поля структуры
-				public string type;
-				public string name;
-				public int age;
+        // Метод класса
+        public void Greetings()
+        {
+            Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+        }
+        // Конструктор 1
+        public Human()
+        {
+            name = "Неизвестно";
+            age = 20;
+        }
+        // Конструктор 2
+        public Human(string n)
+        {
+            name = n;
+            age = 20;
+        }
+        // Конструктор 3
+        public Human(string n, int a)
+        {
+            name = n;
+            age = a;
+        }
 
-				// Метод структуры
-				public void Info()
-				{
-					Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
-				}
-			}
-			class Program
-			{
-				static void Main(string[] args)
-				{
-					Human human = new Human();
-					human.Greetings();
+        struct Animal
+        {
+            // Поля структуры
+            public string type;
+            public string name;
+            public int age;
 
-					human = new Human("Дмитрий");
-					human.Greetings();
+            // Метод структуры
+            public void Info()
+            {
+                Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Human human = new Human();
+                human.Greetings();
 
-					human = new Human("Дмитрий", 23);
-					human.Greetings();
+                human = new Human("Дмитрий");
+                human.Greetings();
 
-					Console.ReadKey();
-				}
-			}
-		}
-	}
+                human = new Human("Дмитрий", 23);
+                human.Greetings();
+
+                // Выполним проверку на null
+                Human human1 = null;
+
+                /* альтернатива блоку if
+                Human human1 = null;
+                Console.WriteLine(human1?.age);*/
+
+                if (human1 != null)
+                {
+                    Console.WriteLine(human1.age);
+                }
+                else
+                {
+                    human1 = new Human();
+                    human1.age = 3;
+                    Console.WriteLine(human1.age);
+                }
+
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+
 }
 
